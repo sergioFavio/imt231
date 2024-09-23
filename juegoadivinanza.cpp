e6a3a2e28bdff0d1d38c570484a2cf90c89166df
sergioFavio <serginho61@gmail.com>
2024-09-23 11:33:21 -0400

    2024-09-23_04 incorporando ciclo while para realizar varios intentos

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
 using namespace std;
-int main (){ 
-cout << "*****************************************" << endl; 
-cout << "* Bienvenido al Juego de la Adivinanza! *" << endl; 
-cout << "*****************************************" << endl; 
+int main (){
+cout << "*****************************************"<<endl;
+cout << "* Bienvenido al Juego de la Adivinanza! *"<<endl;
+cout << "*****************************************"<<endl;
 
-int numero_secreto = 42;
-cout << "el número secreto es ... " << numero_secreto << ". No lo diga a nadie" << endl;
+const int NUMERO_SECRETO = 42;
+int adivina;
+
+bool no_acerto = true;
+
+while (no_acerto){
+
+    cout << "¿cuál es el número? ";
+    cin >> adivina;
+    cout << "El valor de su número es: " << adivina << endl;
+
+    bool acerto = adivina ==  NUMERO_SECRETO;
+    bool mayor = adivina > NUMERO_SECRETO;
+
+   if(acerto){
+       cout << "Felicitaciones!!! ... adivinó el número secreto" << endl;
+       no_acerto = false;
+   }
+
+   else if(mayor){
+       cout << "El número ingresado es mayor que el número secreto" << endl;
+   }
+
+   else{
+       cout << "El número ingresado es menor que el número secreto" << endl;
+   }
+
+
+}
+
+ cout << "Finalizó el juego" << endl;
 
 }
